3639ecb08459e561340d5d6c86ee8a154401e611
Ordinatuer <ordinatuer@yandex.ru>
2024-11-03 23:56:10 +0300

    Изменения для 4-й главы

--- conftest.py
+++ conftest.py
@@ -4,36 +4,19 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--browser_name",
-        action="store",
-        default="chrome",
-        help="Set browser: Chrome or Edge"
-    )
-    parser.addoption(
         "--language",
         action="store",
-        default="uk",
+        default="en",
         help="Set language for testing"
     )
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    browser_name = request.config.getoption("browser_name")
-    browser = None
 
-    if browser_name == "chrome":
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("prefs", {'intl.accept_languages': language})
-
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == "edge":
-        options = webdriver.EdgeOptions()
-        options.add_experimental_option("prefs", {'intl.accept_languages': language})
-
-        browser = webdriver.Edge(options=options)
-    else:
-        raise pytest.UsageError('--browser_name is "chrome" or "edge"')
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
 
     yield browser
     time.sleep(5)
---
+++ test_main_page.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
