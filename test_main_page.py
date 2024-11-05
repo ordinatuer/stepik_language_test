27276cee03456f9fa98650f275e157dbb2627f70
Ordinatuer <ordinatuer@yandex.ru>
2024-11-05 22:15:23 +0300

    Первый тест с PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,12 @@
+from pages.main_page import MainPage
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+
+login_link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, login_link)
+    page.open()
+    page.go_to_login_page()
