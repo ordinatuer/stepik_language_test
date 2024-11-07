4e3874d0171433b1f4a424cc912b227091d9d1c8
Ordinatuer <ordinatuer@yandex.ru>
2024-11-07 19:51:45 +0300

    Продолжение внедрения шаблона Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,15 @@
-# from pages.main_page import MainPage
+from pages.main_page import MainPage
 from pages.login_page import LoginPage
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
-# login_link = "http://selenium1py.pythonanywhere.com/"
-# login_link = "https://selenium1py.pythonanywhere.com/uk/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
-login_link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+link = "https://selenium1py.pythonanywhere.com"
 
 def test_guest_can_go_to_login_page(browser):
-    page = LoginPage(browser, login_link)
+    page = MainPage(browser, link)
     page.open()
+    page.go_to_login_page()
 
-    page.should_be_login_url()
-    page.should_be_login_form()
-    page.should_be_register_form()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
